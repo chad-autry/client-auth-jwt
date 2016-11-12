ce9216bc35a2446860198257280eb0aa8828019d
Chad Autry <chad.autry@gmail.com>
2016-11-12 17:25:21 -0600

    Create defaultOAuth1Config.js

---
+++ src/defaultOAuth1Config.js
@@ -0,0 +1,19 @@
+"use strict";
+/**
+ * The default config properties for OAuth1
+ * @module defaultOAuth1Config
+ */
+
+module.exports = {
+      name: null,
+      url: null,
+      authorizationEndpoint: null,
+      scope: null,
+      scopePrefix: null,
+      scopeDelimiter: null,
+      redirectUri: null,
+      requiredUrlParams: null,
+      defaultUrlParams: null,
+      oauthType: '1.0',
+      popupOptions: { width: null, height: null }
+};
