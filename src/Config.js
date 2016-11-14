cdcb5410746dbb13942a6d422432d8d6cfc6d749
Chad Autry <chad.autry@gmail.com>
2016-11-13 21:50:00 -0600

    Create Config.js

---
+++ src/Config.js
@@ -0,0 +1,19 @@
+"use strict";
+/**
+ * Export the application options.
+ * @module Config
+ */
+
+module.exports = { 
+  baseUrl: '/',
+  loginUrl: '/auth/login',
+  signupUrl: '/auth/signup',
+  unlinkUrl: '/auth/unlink/',
+  tokenName: 'token',
+  tokenPrefix: 'satellizer',
+  tokenHeader: 'Authorization',
+  tokenType: 'Bearer',
+  storageType: 'localStorage',
+  tokenRoot: null,
+  withCredentials: false
+}
