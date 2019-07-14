f6627bd26e80a0ae4b48145713a77bf35542454c
Chad Autry <chad.autry@gmail.com>
2019-07-14 06:49:31 -0500

    Use jwt-simple to decode the payload

--- src/Auth.js
+++ src/Auth.js
@@ -7,10 +7,11 @@
  */
 
 //The storage service
-var defaultConfig = require('./defaultConfig.js');
-var ProviderOAuthConfigs = require('./ProviderOAuthConfigs.js');
-var storage = require('./Storage.js');
-var OAuth = require('./OAuth.js');
+let defaultConfig = require('./defaultConfig.js');
+let ProviderOAuthConfigs = require('./ProviderOAuthConfigs.js');
+let storage = require('./Storage.js');
+let OAuth = require('./OAuth.js');
+let jwt = require('jwt-simple');
  
 /**
  * Auth service, provides the API used
@@ -69,19 +70,17 @@ module.exports = function Auth(originRmiService, config) {
         const token = storage.get(this.prefixedTokenName, 'localStorage');
 
         if (token) {  // Token is present
-            if (token.split('.').length === 3) {  // Token with a valid JWT format XXX.YYY.ZZZ
-                try { // Could be a valid JWT or an access token with the same format
-                    const base64Url = token.split('.')[1];
-                    const base64 = base64Url.replace('-', '+').replace('_', '/');
-                    const exp = JSON.parse(window.atob(base64)).exp;
-                    if (typeof exp === 'number') {  // JWT with an optonal expiration claims
-                        return Math.round(new Date().getTime() / 1000) < exp;
-                    }
-                } catch (e) {
-                    return true;  // Pass: Non-JWT token that looks like JWT
-                }
+            let jwtPayload = {};
+            try {
+                jwtPayload = jwt.decode(token, '', true);
+            } catch (err) {
+                return false;
             }
-            return true;  // Pass: All other tokens
+            if (typeof jwtPayload.exp === 'number') {  // JWT with an optonal expiration claims
+                let now = Math.round(new Date().getTime() / 1000);
+                return jwtPayload.exp < now;
+            }
+            
         }
         return false; // Fail: No token at all
     };
@@ -92,16 +91,16 @@ module.exports = function Auth(originRmiService, config) {
     this.getPayload = () => {
         const token = storage.get(auth.prefixedTokenName, 'localStorage');
 
-        if (token && token.split('.').length === 3) {
+        if (token) {
+            let jwtPayload = {};
             try {
-                const base64Url = token.split('.')[1];
-                const base64 = base64Url.replace('-', '+').replace('_', '/');
-                const payload = JSON.parse(decodeBase64(base64));
-                return payload;
-            } catch (e) {
+                jwtPayload = jwt.decode(token, '', true);
+            } catch (err) {
                 return {};
             }
+            return jwtPayload;
         }
+        return {};
     };
 
    /**
@@ -134,67 +133,3 @@ module.exports = function Auth(originRmiService, config) {
     */
     this.setStorageType = (type) => storage.setStorageType(type);
 };
-
-function decodeBase64 (str) {
-    let buffer;
-    if (typeof module !== 'undefined' && module.exports) {
-        try {
-            buffer = require('buffer').Buffer;
-        } catch (err) {
-            // noop
-        }
-    }
-
-    let fromCharCode = String.fromCharCode;
-
-    let re_btou = new RegExp([
-        '[\xC0-\xDF][\x80-\xBF]',
-        '[\xE0-\xEF][\x80-\xBF]{2}',
-        '[\xF0-\xF7][\x80-\xBF]{3}'
-    ].join('|'), 'g');
-
-    let cb_btou = function (cccc) {
-        switch (cccc.length) {
-            case 4: {
-                let cp = ((0x07 & cccc.charCodeAt(0)) << 18)
-                    | ((0x3f & cccc.charCodeAt(1)) << 12)
-                    | ((0x3f & cccc.charCodeAt(2)) << 6)
-                    | (0x3f & cccc.charCodeAt(3));
-                let offset = cp - 0x10000;
-                return (fromCharCode((offset >>> 10) + 0xD800)
-                    + fromCharCode((offset & 0x3FF) + 0xDC00));
-            }
-            case 3: {
-                return fromCharCode(
-                    ((0x0f & cccc.charCodeAt(0)) << 12)
-                    | ((0x3f & cccc.charCodeAt(1)) << 6)
-                    | (0x3f & cccc.charCodeAt(2))
-                );
-            }
-            default: {
-                return fromCharCode(
-                    ((0x1f & cccc.charCodeAt(0)) << 6)
-                    | (0x3f & cccc.charCodeAt(1))
-                );
-            }
-        }
-    };
-
-    let btou = function (b) {
-        return b.replace(re_btou, cb_btou);
-    };
-
-    let _decode = buffer ? function (a) {
-        return (a.constructor === buffer.constructor
-            ? a : new buffer(a, 'base64')).toString();
-    }
-    : function (a) {
-        return btou(atob(a));
-    };
-
-    return _decode(
-        String(str).replace(/[-_]/g, function (m0) {
-            return m0 === '-' ? '+' : '/';
-        }).replace(/[^A-Za-z0-9\+\/]/g, '')
-    );
-}
